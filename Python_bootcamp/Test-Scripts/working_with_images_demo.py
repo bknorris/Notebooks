fb7bbe05745ecdef457eab1420b9eb5b061ee8a9
bknorris <bnorris0600@gmail.com>
2022-02-23 21:31:32 -0800

    Began Section 16

---
+++ Python_bootcamp/Test-Scripts/working_with_images_demo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Demo for image processing, based on the
+Complete-python-bootcamp on udemy.com
+
+
+"""
+
+path = 'c:\\Users\\bknorris\\Documents\\Scripts\\Python\\Python_bootcamp\\Complete-Python-3-Bootcamp\\14-Working-with-Images\\'
+
